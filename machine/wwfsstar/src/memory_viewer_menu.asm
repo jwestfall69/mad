7b34068cb9cbd3f0805a0ba6017f92cc5265c7c5
Jim Westfall <commits@jwestfall.net>
2024-04-30 18:59:13 -0700

    wwfsstar: fill out memory viewer menu

--- machine/wwfsstar/src/memory_viewer_menu.asm
+++ machine/wwfsstar/src/memory_viewer_menu.asm
@@ -28,8 +28,28 @@ memory_viewer_menu:
 
 		rts
 
+view_bg_ram:
+		lea	BG_RAM_START, a0
+		bra	view_ram
+
+view_fg_ram:
+		lea	FG_RAM_START, a0
+		bra	view_ram
+
+view_mmio:
+		lea	$180000, a0
+		bra	view_ram
+
 view_palette_ram:
 		lea	PALETTE_RAM_START, a0
+		bra	view_ram
+
+view_rom_space:
+		lea	$0, a0
+		bra	view_ram
+
+view_sprite_ram:
+		lea	SPRITE_RAM_START, a0
 		bra	view_ram
 
 view_work_ram:
@@ -48,11 +68,21 @@ STR_MENU_TITLE:		STRING "MEMORY VIEW MENU"
 	align 2
 
 MENU_LIST:
-	MENU_ENTRY view_work_ram, STR_WORK_RAM
+	MENU_ENTRY view_bg_ram, STR_BG_RAM
+	MENU_ENTRY view_fg_ram, STR_FG_RAM
+	MENU_ENTRY view_mmio, STR_MMIO
 	MENU_ENTRY view_palette_ram, STR_PALETTE_RAM
+	MENU_ENTRY view_rom_space, STR_ROM_SPACE
+	MENU_ENTRY view_sprite_ram, STR_SPRITE_RAM
+	MENU_ENTRY view_work_ram, STR_WORK_RAM
 	MENU_LIST_END
 
 
-STR_WORK_RAM:		STRING "WORK RAM"
+STR_BG_RAM:		STRING "BG RAM"
+STR_FG_RAM:		STRING "FG RAM"
+STR_MMIO:		STRING "MMIO"
 STR_PALETTE_RAM:	STRING "PALETTE RAM"
+STR_ROM_SPACE:		STRING "ROM SPACE"
+STR_SPRITE_RAM:		STRING "SPRITE RAM"
+STR_WORK_RAM:		STRING "WORK RAM"
 
