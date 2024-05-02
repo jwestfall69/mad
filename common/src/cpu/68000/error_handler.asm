0ad5cb6b27b6b8d373ebb7365c22fb2b9a284c4f
Jim Westfall <commits@jwestfall.net>
2024-05-01 21:39:43 -0700

    return control back to error_handler after print_error function
    This will be needed so we can have error_handler call a function
    that will play the error code via the sound cpu.

--- common/src/cpu/68000/error_handler.asm
+++ common/src/cpu/68000/error_handler.asm
@@ -1,5 +1,6 @@
 	include "cpu/68000/dsub.inc"
 	include "cpu/68000/error_handler.inc"
+	include "cpu/68000/macros.inc"
 	include "cpu/68000/print_error.inc"
 
 	global error_handler_dsub
@@ -30,29 +31,39 @@ error_handler_dsub:
 		; failed to find error code !?
 		move.b	#PRINT_ERROR_INVALID, d1
 		lea	print_error_invalid_dsub, a2
-		bra	.pf_run
+		bra	.pe_run
 
 	.ec_found:
-		move.b  (1, a1), d4	; print error dsub id
+		move.b	(1, a1), d4	; print error dsub id
 		movea.l	(2, a1), a1	; error description string
 		and.w	#$ff, d4
 
 		; use the print error dsub id to find print error dsub to run
 		lea	(EC_PRINT_LIST), a2
-	.loop_pf_next_entry:
+	.loop_pe_next_entry:
 		cmp.w	(a2), d4
-		beq 	.pf_found
+		beq 	.pe_found
 		addq.l	#6, a2
 		tst.l	(a2)			; table is null terminated
-		bne	.loop_pf_next_entry
+		bne	.loop_pe_next_entry
 
 		; no print function found !?
 		lea	print_error_invalid_dsub, a2
 		move.b	d4, d1
-		bra	.pf_run
+		bra	.pe_run
 
-	.pf_found:
+	.pe_found:
 		movea.l	(2, a2), a2
 
-	.pf_run:
-		jmp	(a2)		; pf will DSUB_RETURN
+	.pe_run:
+		move.b	d0, d6		; backup error code
+
+		; manually do dsub based on the print function,
+		; which is already in a2
+		lea	.pe_return, a3
+		bra	dsub_enter
+
+	.pe_return:
+		move.b	d6, d0
+		; todo add play error
+		STALL
