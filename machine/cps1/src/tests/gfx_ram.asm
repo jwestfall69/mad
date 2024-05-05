cc0108a5c0caa56a9b646f21ce44cac2c827df0b
Jim Westfall <commits@jwestfall.net>
2024-05-04 21:11:41 -0700

    cps1: switch to memory_tests_handler, hookup gfx ram in main menu

--- machine/cps1/src/tests/gfx_ram.asm
+++ machine/cps1/src/tests/gfx_ram.asm
@@ -1,5 +1,6 @@
 	include "cpu/68000/dsub.inc"
 	include "cpu/68000/macros.inc"
+	include "cpu/68000/memory_tests_handler.inc"
 	include "cpu/68000/tests/memory.inc"
 	include "cpu/68000/xy_string.inc"
 
@@ -13,65 +14,9 @@
 	section code
 
 auto_gfx_ram_tests:
-		lea	MEMORY_ADDRESS_LIST, a0
-		moveq	#0, d0
-		RSUB	memory_output_list_test
-		tst.b	d0
-		bne	.test_failed_output
-
-		lea	MEMORY_ADDRESS_LIST, a0
-		moveq	#0, d0
-		RSUB	memory_write_list_test
-		tst.b	d0
-		bne	.test_failed_write
-
-		lea	GFX_RAM_START, a0
-		move.l	#GFX_RAM_SIZE, d0
-		move.w	#$ffff, d1
-		RSUB	memory_data_test
-		tst.b	d0
-		bne	.test_failed_data
-
-		lea	GFX_RAM_START, a0
-		move.w	#GFX_RAM_ADDRESS_LINES, d0
-		move.w	#$ffff, d1
-		RSUB	memory_address_test
-		tst.b	d0
-		bne	.test_failed_address
-
-		lea	GFX_RAM_START, a0
-		move.l	#GFX_RAM_SIZE, d0
-		move.w	#$ffff, d1
-		RSUB	memory_march_test
-		tst.b	d0
-		bne	.test_failed_march
+		lea	MT_DATA, a0
+		DSUB	memory_tests_handler
 		rts
-
-	.test_failed_address:
-		moveq	#EC_GFX_RAM_ADDRESS, d0
-		rts
-
-	.test_failed_data:
-		subq.b	#1, d0
-		add.b	#EC_GFX_RAM_DATA_LOWER, d0
-		rts
-
-	.test_failed_march:
-		subq.b	#1, d0
-		add.b	#EC_GFX_RAM_MARCH_LOWER, d0
-		rts
-
-	.test_failed_output:
-		subq.b	#1, d0
-		add.b	#EC_GFX_RAM_OUTPUT_LOWER, d0
-		rts
-
-	.test_failed_write:
-		subq.b	#1, d0
-		add.b	#EC_GFX_RAM_WRITE_LOWER, d0
-		rts
-
-
 
 manual_gfx_ram_tests:
 
@@ -82,20 +27,33 @@ manual_gfx_ram_tests:
 
 	.loop_next_pass:
 
-		SEEK_XY	12, 10
-		move.l	d6, d0
-		RSUB	print_hex_long
-
-
 		bsr	auto_gfx_ram_tests
 		tst.b	d0
 		bne	.test_failed
+
+		btst	#P1_B1_BIT, REG_INPUT_P1
+		beq	.test_pause
 
 		btst	#P1_B2_BIT, REG_INPUT_P1
 		beq	.test_exit
 
 		addq.l	#1, d6
 
+		bra	.loop_next_pass
+
+	.test_pause:
+		RSUB	screen_init
+
+		lea	SCREEN_XYS_LIST, a0
+		RSUB	print_xy_string_list
+
+		SEEK_XY	12, 10
+		move.l	d6, d0
+		RSUB	print_hex_long
+
+	.loop_paused:
+		btst	#P1_B1_BIT, REG_INPUT_P1
+		beq	.loop_paused
 		bra	.loop_next_pass
 
 	.test_failed:
@@ -107,13 +65,19 @@ manual_gfx_ram_tests:
 
 	section data
 
-SCREEN_XYS_LIST:
-	XY_STRING 3, 10, "PASSES"
-	XY_STRING 3, 20, "B2 - RETURN TO MENU"
-	XY_STRING_LIST_END
+	align 2
 
 ; fix me based on ram chips
-	align 2
 MEMORY_ADDRESS_LIST:
 	MEMORY_ADDRESS_ENTRY GFX_RAM_START
 	MEMORY_ADDRESS_LIST_END
+
+MT_DATA:
+	MT_PARAMS GFX_RAM_START, MEMORY_ADDRESS_LIST, GFX_RAM_SIZE, GFX_RAM_ADDRESS_LINES, GFX_RAM_MASK, $0, GFX_RAM_BASE_EC
+
+SCREEN_XYS_LIST:
+	XY_STRING 3,  4, "GFX RAM TEST"
+	XY_STRING 3, 10, "PASSES"
+	XY_STRING 3, 19, "B1 - PAUSE"
+	XY_STRING 3, 20, "B2 - RETURN TO MENU"
+	XY_STRING_LIST_END
