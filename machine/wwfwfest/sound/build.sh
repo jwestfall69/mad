ce00ec1363d247487eee57d320b8d128d341f191
Jim Westfall <commits@jwestfall.net>
2024-08-17 16:58:38 -0700

    Add wwfwfest sound diag

---
+++ machine/wwfwfest/sound/build.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+make -f Makefile clean && make -f Makefile && cp build/31a11* ../../../../mame/roms/wwfwfest/
---
+++ machine/wwfwfest/sound/src/tests/msm6295.asm
@@ -0,0 +1,34 @@
+	include "cpu/z80/include/macros.inc"
+	include "cpu/z80/include/psub.inc"
+
+	include "error_codes.inc"
+	include "machine.inc"
+
+	global msm6295_tests
+	section code
+
+msm6295_tests:
+
+		ld	hl, REG_MSM6295
+		PSUB	memory_output_test
+		jr	z, .test_passed_output
+		ld	a, EC_MSM6295_OUTPUT
+		jp	error_address
+
+	.test_passed_output:
+		ld	hl, REG_MSM6295
+		call	msm6295_not_playing_test
+		jr	z, .test_passed_not_playing
+		ld	a, EC_MSM6295_ALREADY_PLAYING
+		jp	error_address
+
+	.test_passed_not_playing:
+		ld	hl, REG_MSM6295
+		ld	a, $2			; sound number to try playing
+		call	msm6295_play_test
+		jr	z, .test_passed_play
+		ld	a, EC_MSM6295_PLAY
+		jp	error_address
+
+	.test_passed_play:
+		ret
