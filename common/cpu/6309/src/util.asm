3a4d23ad2880c011944302a99e36660275497dff
Jim Westfall <commits@jwestfall.net>
2024-09-07 17:24:58 -0700

    6309/mainevt: add code to play beep codes

--- common/cpu/6309/src/util.asm
+++ common/cpu/6309/src/util.asm
@@ -1,6 +1,10 @@
 	include "cpu/6309/include/psub.inc"
 
+	include "machine.inc"
+	include "mad_rom.inc"
+
 	global delay_psub
+	global sound_play_byte_psub
 
 	section code
 
@@ -19,4 +23,34 @@ delay_psub:
 		cmpd	#0		; 4 cycles
 		decw			; 3 cycles
 		bne	 delay_psub 	; 2 cycles
+		PSUB_RETURN
+
+; params:
+;  a = byte to play
+sound_play_byte_psub:
+		tfr	a, b
+		lde	#$8
+
+	.loop_next_bit:
+		WATCHDOG
+
+		lslb
+		bcc	.is_zero
+
+		lda	#SOUND_NUM_BIT_ONE
+		bra	.sound_play
+
+	.is_zero:
+		lda	#SOUND_NUM_BIT_ZERO
+
+	.sound_play:
+		SOUND_PLAY
+		tfr	d, x
+		tfr	w, y
+		SOUND_BIT_DELAY
+		tfr	x, d
+		tfr	y, w
+
+		dece
+		bne	.loop_next_bit
 		PSUB_RETURN
