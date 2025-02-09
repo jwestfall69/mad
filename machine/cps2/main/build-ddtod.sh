2ad3903cd715a6457b9c688e7496d826361076c0
Jim Westfall <commits@jwestfall.net>
2025-02-09 15:52:26 -0800

    cps2: add ddtod romset

---
+++ machine/cps2/main/build-ddtod.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+make -f Makefile.ddtod clean && make -f Makefile.ddtod && cp build/ddtod/encrypted-dade.03c ../../../../mame/roms/ddtod/dade.03c && cp build/ddtod/suicide-dade.03c ../../../../mame/roms/ddtodd/daded.03c
