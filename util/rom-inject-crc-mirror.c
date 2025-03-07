ff8f17e597112857a76db38a7eb9462e4501a65c
Jim Westfall <commits@jwestfall.net>
2025-03-06 18:50:15 -0800

    rom-inject-crc-mirror: add support for crc16 (xmodem) via -x option
    This will used with the soon to be added 6809 cpu, which doesn't have
    enough registers to calculate crc32 without touching ram.

--- util/rom-inject-crc-mirror.c
+++ util/rom-inject-crc-mirror.c
@@ -6,15 +6,19 @@
 #include <sys/stat.h>
 
 // negative offsets from end
-#define DEFAULT_CRC32_OFFSET        4
+#define DEFAULT_CRC_OFFSET          4
 #define DEFAULT_MIRROR_OFFSET       5
 
 #define DEFAULT_START_OFFSET        0
 
-#define CRC32_BIG_ENDIAN            0
-#define CRC32_LITTLE_ENDIAN         1
+#define CRC_BIG_ENDIAN              0
+#define CRC_LITTLE_ENDIAN           1
+
+#define CRC_TYPE_CRC16              0
+#define CRC_TYPE_CRC32              1
 
 int check_size(uint32_t size);
+uint16_t crc16_calc(const void *data, size_t n_bytes);
 uint32_t crc32_calc(const void *data, size_t n_bytes);
 uint32_t crc32_for_byte(uint32_t r);
 void usage(void);
@@ -24,7 +28,9 @@ int main(int argc, char **argv) {
   FILE *rom = NULL;
   char *data;
   uint32_t crc32;
-  uint16_t crc32_offset = DEFAULT_CRC32_OFFSET;
+  uint16_t crc16;
+  uint16_t crc_offset = DEFAULT_CRC_OFFSET;
+  uint8_t crc_type = CRC_TYPE_CRC32;
   uint8_t mirror = 0;
   uint16_t mirror_offset = DEFAULT_MIRROR_OFFSET;
   int32_t target_size = -1;
@@ -35,18 +41,18 @@ int main(int argc, char **argv) {
   struct stat sb;
   int32_t endian = -1;
 
-  while((opt = getopt(argc, argv, "c:e:f:hm:rs:t:")) != -1) {
+  while((opt = getopt(argc, argv, "c:e:f:hm:rs:t:x")) != -1) {
     switch (opt) {
 
       case 'c':
-          crc32_offset = atoi(optarg);
+          crc_offset = atoi(optarg);
           break;
 
       case 'e':
         if(strcmp("big", optarg) == 0) {
-          endian = CRC32_BIG_ENDIAN;
+          endian = CRC_BIG_ENDIAN;
         } else if(strcmp("little", optarg) == 0) {
-          endian = CRC32_LITTLE_ENDIAN;
+          endian = CRC_LITTLE_ENDIAN;
         } else {
           printf("ERROR: -e arg must either be \'big\' for big-endian or \'little\' for little-endian\n");
           exit(EXIT_FAILURE);
@@ -90,6 +96,10 @@ int main(int argc, char **argv) {
 
       case 's':
         start_offset = atoi(optarg);
+        break;
+
+      case 'x':
+        crc_type = CRC_TYPE_CRC16;
         break;
 
       case 'h':
@@ -145,24 +155,43 @@ int main(int argc, char **argv) {
 
   printf("Start Size:    0x%x\n", start_size);
   printf("Target Size:   0x%x\n", target_size);
-  printf("CRC32 Offset:  0x%02x from end\n", crc32_offset);
+  printf("CRC Offset:    0x%02x from end\n", crc_offset);
   printf("Mirror Offset: 0x%02x from end\n", mirror_offset);
 
-  // don't calc crc of the mirror/crc part of the rom
-  crc32 = crc32_calc(data + start_offset, (start_size - start_offset) - mirror_offset);
-  printf("CRC32 Range:   0x%x - 0x%x\n", start_offset, start_size - mirror_offset);
-  printf("CRC32:         0x%8x\n", crc32);
 
-  if(endian == CRC32_BIG_ENDIAN) {
-    crc32 = htobe32(crc32);
+  if(crc_type == CRC_TYPE_CRC32) {
+    // don't calc crc of the mirror/crc part of the rom
+    crc32 = crc32_calc(data + start_offset, (start_size - start_offset) - mirror_offset);
+    printf("CRC32 Range:   0x%x - 0x%x\n", start_offset, start_size - mirror_offset);
+    printf("CRC32:         0x%08x\n", crc32);
+
+    if(endian == CRC_BIG_ENDIAN) {
+      crc32 = htobe32(crc32);
+    } else {
+      crc32 = htole32(crc32);
+    }
   } else {
-    crc32 = htole32(crc32);
+    // don't calc crc of the mirror/crc part of the rom
+    crc16 = crc16_calc(data + start_offset, (start_size - start_offset) - mirror_offset);
+    printf("CRC16 Range:   0x%x - 0x%x\n", start_offset, start_size - mirror_offset);
+    printf("CRC16:         0x%04x\n", crc16);
+
+    if(endian == CRC_BIG_ENDIAN) {
+      crc16 = htobe16(crc16);
+    } else {
+      crc16 = htole16(crc16);
+    }
   }
 
   // in reverse mode the last copy is the active one
   if(reverse) {
     data[target_size - mirror_offset] = mirror;
-    memcpy((data + target_size) - crc32_offset, &crc32, 4);
+
+    if(crc_type == CRC_TYPE_CRC32) {
+      memcpy((data + target_size) - crc_offset, &crc32, 4);
+    } else {
+      memcpy((data + target_size) - crc_offset, &crc16, 2);
+    }
 
     offset = target_size - (2 * start_size);
 
@@ -176,8 +205,12 @@ int main(int argc, char **argv) {
   } else {
     // fill in the first one manaully since crc32 is only in it
     data[start_size - mirror_offset] = mirror;
-    memcpy((data + start_size) - crc32_offset, &crc32, 4);
 
+    if(crc_type == CRC_TYPE_CRC32) {
+      memcpy((data + start_size) - crc_offset, &crc32, 4);
+    } else {
+      memcpy((data + start_size) - crc_offset, &crc16, 2);
+    }
     offset = start_size;
 
     // fill in mirror numbers
@@ -197,6 +230,21 @@ int main(int argc, char **argv) {
 
 int check_size(uint32_t size) {
   return ((size & (size - 1)) == 0);
+}
+
+uint16_t crc16_calc(const void *data, size_t n_bytes) {
+  uint32_t crc = 0;
+
+  for(int i = 0; i < n_bytes; i++) {
+    crc = crc ^ (((uint8_t*)data)[i] << 8);
+    for(int i = 0; i < 8; i++) {
+      crc = crc << 1;
+      if(crc & 0x10000) {
+        crc = (crc ^ 0x1021) & 0xFFFF;
+      }
+    }
+  }
+  return (uint16_t)crc;
 }
 
 uint32_t crc32_for_byte(uint32_t r) {
@@ -228,7 +276,8 @@ void usage(void) {
   printf("  -t <target_size>           - how large the <romfile> should become [required]\n");
   printf("  -e <big|little>            - use big or little endian when writing crc32 value [required]\n");
   printf("  -r                         - reverse mode, the last copy is the active one in the rom\n");
-  printf("  -s <crc32 start offset>    - offset from beginning of rom to start crc32 calc [default: %d]\n", DEFAULT_START_OFFSET);
-  printf("  -c <crc32 write offset>    - offset from end of rom of where to write the crc32 data [default: %d]\n", DEFAULT_CRC32_OFFSET);
+  printf("  -s <crc start offset>      - offset from beginning of rom to start crc calc [default: %d]\n", DEFAULT_START_OFFSET);
+  printf("  -c <crc write offset>      - offset from end of rom of where to write the crc data [default: %d]\n", DEFAULT_CRC_OFFSET);
   printf("  -m <mirror write offset>   - offset from end of rom of where to write the mirror data [default: %d]\n", DEFAULT_MIRROR_OFFSET);
+  printf("  -x                         - use crc16 (xmodem) instead of crc32\n");
 }
