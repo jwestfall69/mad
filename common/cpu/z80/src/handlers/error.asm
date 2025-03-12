fc0d681f3a20e571f4dc3004f78faaf8c3498a91
Jim Westfall <commits@jwestfall.net>
2025-03-11 19:01:34 -0700

    z80: add print_error and error handler

---
+++ common/cpu/z80/src/handlers/error.asm
@@ -0,0 +1,100 @@
+	include "cpu/z80/include/dsub.inc"
+	include "cpu/z80/include/macros.inc"
+	include "cpu/z80/include/print_error.inc"
+	include "cpu/z80/include/handlers/error.inc"
+
+	include "machine.inc"
+	include "mad.inc"
+
+	global error_handler
+
+	section code
+
+; based on the error code, figure out which print error
+; function should be called and call it
+; params:
+;  a = error code
+;  b = error data
+;  c = error data
+;  hl = error data
+error_handler:
+		ld	(r_pe_data_a), a
+		ld	a, b
+		ld	(r_pe_data_b), a
+		ld	a, c
+		ld	(r_pe_data_c), a
+		ld	(r_pe_data_hl), hl
+
+		; screen maybe messed up depending
+		; on which test is running
+		RSUB	screen_init
+
+		ld	ix, d_ec_list
+		ld	a, (r_pe_data_a)
+		ld	e, a
+	.loop_ec_next_entry:
+		ld	a, e
+		cp	(ix + s_ee_error_code)
+		jr	z, .ec_found
+
+		ld	bc, s_ee_struct_size
+		add	ix, bc
+
+		xor	a
+		cp	(ix + s_ee_error_code)
+		jr	nz, .loop_ec_next_entry
+
+		; failed to find error code !?
+		ld	a, PRINT_ERROR_INVALID
+		ld	(r_pe_data_b), a
+		ld	hl, print_error_invalid
+		jr	.pe_run
+
+	.ec_found:
+		ld	l, (ix + s_ee_description_ptr)
+		ld	h, (ix + s_ee_description_ptr + 1)
+		ld	(r_pe_string_ptr), hl
+		ld	e, (ix + s_ee_print_error_id)
+
+		ld	iy, d_ec_print_list
+	.loop_pe_next_entry:
+		ld	a, e
+		cp	(iy + s_pe_print_error_id)
+		jr	z, .pe_found
+
+		ld	bc, s_pe_struct_size
+		add	iy, bc
+
+		xor	a
+		cp	(iy + s_pe_print_error_id)
+		jr	nz, .loop_pe_next_entry
+
+		; no print function found !?
+		ld	a, e
+		ld	(r_pe_data_b), a
+		ld	hl, print_error_invalid
+		jr	.pe_run
+
+	.pe_found:
+		ld	l, (iy + s_pe_function_ptr)
+		ld	h, (iy + s_pe_function_ptr + 1)
+
+	.pe_run:
+		; setup stack so we ret to after the jp
+		ld	de, .pe_return
+		push	de
+		jp	(hl)
+	.pe_return:
+
+
+	;	ld	a, (r_pe_data_a)
+	;	RSUB	sound_play_byte
+
+	ifnd _ERROR_ADDRESS_DISABLED_
+		ld	a, (r_pe_data_a)
+		jp	error_address
+	else
+		STALL
+	endif
+
+		STALL
---
+++ common/cpu/z80/src/print_error.asm
@@ -0,0 +1,182 @@
+	include "global/include/macros.inc"
+	include "global/include/screen.inc"
+	include "cpu/z80/include/dsub.inc"
+	include "cpu/z80/include/macros.inc"
+	include "cpu/z80/include/print_error.inc"
+
+	include "machine.inc"
+
+	global print_error_address
+	global print_error_hex_byte
+	global print_error_invalid
+	global print_error_memory
+	global print_error_string
+	global d_ec_print_list
+	global d_str_actual
+	global d_str_address
+	global d_str_expected
+	global r_pe_data_a
+	global r_pe_data_b
+	global r_pe_data_c
+	global r_pe_data_hl
+	global r_pe_string_ptr
+
+	section code
+
+; these are print_error functions that get called by error_handler
+
+; params:
+;  r_pe_data_hl = address
+;  r_pe_string_ptr = error description
+print_error_address:
+		; error
+		SEEK_XY	SCREEN_START_X, SCREEN_START_Y
+		ld	de, (r_pe_string_ptr)
+		RSUB	print_string
+
+		; address
+		SEEK_XY	(SCREEN_START_X + 10), (SCREEN_START_Y + 2)
+		ld	bc, (r_pe_data_hl)
+		RSUB	print_hex_word
+
+		SEEK_XY	SCREEN_START_Y, (SCREEN_START_Y + 2)
+		ld	de, d_str_address
+		RSUB	print_string
+		ret
+
+; params:
+;  r_pe_data_b = expected value
+;  r_pe_data_c = actual value
+;  r_pe_string_ptr = error description
+print_error_hex_byte:
+		; error
+		SEEK_XY	SCREEN_START_X, SCREEN_START_Y
+		ld	de, (r_pe_string_ptr)
+		RSUB	print_string
+
+		; expected
+		SEEK_XY (SCREEN_START_X + 12), (SCREEN_START_Y + 2)
+		ld	a, (r_pe_data_b)
+		ld	c, a
+		RSUB	print_hex_byte
+
+		; actual
+		SEEK_XY	(SCREEN_START_X + 12), (SCREEN_START_Y + 3)
+		ld	a, (r_pe_data_c)
+		ld	c, a
+		RSUB	print_hex_byte
+
+		SEEK_XY	SCREEN_START_X, (SCREEN_START_Y + 2)
+		ld	de, d_str_expected
+		RSUB	print_string
+
+		SEEK_XY	SCREEN_START_X, (SCREEN_START_Y + 3)
+		ld	de, d_str_actual
+		RSUB	print_string
+		ret
+
+; params:
+;  r_pe_data_a = error code
+;  r_pe_data_b = function id
+print_error_invalid:
+		; error code
+		SEEK_XY	(SCREEN_START_X + 16), (SCREEN_START_Y + 2)
+		ld	a, (r_pe_data_a)
+		ld	c, a
+		RSUB	print_hex_byte
+
+		; function id
+		SEEK_XY	(SCREEN_START_X + 16), (SCREEN_START_Y + 3)
+		ld	a, (r_pe_data_b)
+		ld	c, a
+		RSUB	print_hex_byte
+
+		SEEK_XY	SCREEN_START_X, SCREEN_START_Y
+		ld	de, d_str_invalid_error
+		RSUB	print_string
+
+		SEEK_XY	SCREEN_START_X, (SCREEN_START_Y + 2)
+		ld	de, d_str_error_code
+		RSUB	print_string
+
+		SEEK_XY	SCREEN_START_X, (SCREEN_START_Y + 3)
+		ld	de, d_str_print_function
+		RSUB	print_string
+
+		ret
+
+; params:
+;  r_pe_data_b = expected value
+;  r_pe_data_c = actual value
+;  r_pe_data_hl = address
+;  r_pe_string_ptr = error description
+print_error_memory:
+		; description
+		SEEK_XY	SCREEN_START_X, SCREEN_START_Y
+		ld	de, (r_pe_string_ptr)
+		RSUB	print_string
+
+		; address
+		SEEK_XY	(SCREEN_START_X + 10), (SCREEN_START_Y + 2)
+		ld	bc, (r_pe_data_hl)
+		RSUB	print_hex_word
+
+		; expected
+		SEEK_XY	(SCREEN_START_X + 12), (SCREEN_START_Y + 3)
+		ld	a, (r_pe_data_b)
+		ld	c, a
+		RSUB	print_hex_byte
+
+		; actual
+		SEEK_XY	(SCREEN_START_X + 12), (SCREEN_START_Y + 4)
+		ld	a, (r_pe_data_c)
+		ld	c, a
+		RSUB	print_hex_byte
+
+		SEEK_XY	SCREEN_START_X, (SCREEN_START_Y + 2)
+		ld	de, d_str_address
+		RSUB	print_string
+
+		SEEK_XY	SCREEN_START_X, (SCREEN_START_Y + 3)
+		ld	de, d_str_expected
+		RSUB	print_string
+
+		SEEK_XY	SCREEN_START_X, (SCREEN_START_Y + 4)
+		ld	de, d_str_actual
+		RSUB	print_string
+		ret
+
+; params:
+;  r_pe_string_ptr = error description
+print_error_string:
+		SEEK_XY	SCREEN_START_X, SCREEN_START_Y
+		ld	de, (r_pe_string_ptr)
+		RSUB	print_string
+		ret
+
+	section data
+
+d_ec_print_list:
+	EC_PRINT_ENTRY PRINT_ERROR_ADDRESS, print_error_address
+	EC_PRINT_ENTRY PRINT_ERROR_HEX_BYTE, print_error_hex_byte
+	EC_PRINT_ENTRY PRINT_ERROR_MEMORY, print_error_memory
+	EC_PRINT_ENTRY PRINT_ERROR_STRING, print_error_string
+	EC_PRINT_LIST_END
+
+d_str_address:		STRING "ADDRESS", CHAR_COLON
+d_str_actual:		STRING "ACTUAL", CHAR_COLON
+d_str_expected:		STRING "EXPECTED", CHAR_COLON
+
+d_str_invalid_error:	STRING "INVALID ERROR OR PRINT CODE"
+d_str_error_code:	STRING "ERROR CODE", CHAR_COLON
+d_str_print_function:	STRING "PRINT FUNCTION", CHAR_COLON
+
+	section bss
+
+; error handler will populate these before calling
+; the necessary print_error function.
+r_pe_data_a:		dc.b $0
+r_pe_data_b:		dc.b $0
+r_pe_data_c:		dc.b $0
+r_pe_data_hl:		dc.w $0
+r_pe_string_ptr:	dc.w $0
