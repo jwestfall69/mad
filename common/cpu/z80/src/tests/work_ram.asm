c7f7e60e1061be30bdb41846e0720d3c8cc4c223
Jim Westfall <commits@jwestfall.net>
2025-03-12 18:21:26 -0700

    z80: add manual work ram tests

--- common/cpu/z80/src/tests/work_ram.asm
+++ common/cpu/z80/src/tests/work_ram.asm
@@ -1,14 +1,26 @@
+	include "global/include/macros.inc"
+	include "global/include/screen.inc"
 	include "cpu/z80/include/macros.inc"
 	include "cpu/z80/include/dsub.inc"
+	include "cpu/z80/include/xy_string.inc"
 
 	include "error_codes.inc"
+	include "input.inc"
 	include "machine.inc"
+	include "mad.inc"
 
 	global auto_work_ram_tests_dsub
+
+	ifnd _HEADLESS_
+
+	global manual_work_ram_tests
+
+	endif
 
 	section code
 
 auto_work_ram_tests_dsub:
+		exx
 
 		ld	hl, WORK_RAM_START
 		PSUB	memory_output_test
@@ -69,3 +81,48 @@ auto_work_ram_tests_dsub:
 	.test_failed_data:
 		ld	a, EC_WORK_RAM_DATA
 		jp	error_address
+
+	ifnd _HEADLESS_
+
+manual_work_ram_tests:
+		ld	de, d_screen_xys_list
+		call	print_xy_string_list
+
+		ld	bc, 0
+		ld	(R_WORK_RAM_PASSES), bc
+
+		DSUB_MODE_PSUB
+
+	.loop_next_pass:
+		SEEK_XY	SCREEN_PASSES_VALUE_X, SCREEN_PASSES_Y
+		ld	bc, (R_WORK_RAM_PASSES)
+		PSUB	print_hex_word
+
+		PSUB	auto_work_ram_tests
+
+		ld	b, INPUT_B2
+		PSUB	check_button_press
+		cp	1
+		jr	z, .test_exit
+
+		ld	bc, (R_WORK_RAM_PASSES)
+		inc	bc
+		ld	(R_WORK_RAM_PASSES), bc
+		jr	.loop_next_pass
+
+	.test_exit:
+		ld	a, 0
+		ld	(r_menu_cursor), a
+
+		DSUB_MODE_RSUB
+
+		jp	main_menu
+
+	section data
+
+d_screen_xys_list:
+	XY_STRING SCREEN_START_X, SCREEN_PASSES_Y, "PASSES"
+	XY_STRING SCREEN_START_X, SCREEN_B2_Y, "B2 - RETURN TO MENU"
+	XY_STRING_LIST_END
+
+	endif
