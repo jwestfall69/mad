d8c4cee173d5084c1505431f4979fc959d7bb7f3
Jim Westfall <commits@jwestfall.net>
2025-05-10 15:52:09 -0700

    konami2: add joystick_update_(byte|word) helper functions
    These take a byte|word location in memory and inc/dec the value
    based on joystick input

--- common/cpu/konami2/src/util.asm
+++ common/cpu/konami2/src/util.asm
@@ -5,6 +5,8 @@
 	include "mad.inc"
 
 	global delay
+	global joystick_update_byte
+	global joystick_update_word
 	global wait_button_press
 	global wait_button_release
 
@@ -26,9 +28,107 @@ delay:
 		cmpd	#0		; 4 cycles
 		cmpd	#0		; 4 cycles
 		subd	#$1
-		bne	 delay	 	; 2 cycles
+		bne	delay	 	; 2 cycles
 		rts
 
+
+; Looking at input data, adjust the byte in memory by
+;  +$1 for UP
+;  -$1 for DOWN
+; -$10 for LEFT
+; +$10 for RIGHT
+; Then apply the mask to the byte
+; params:
+;  a = mask
+;  x = #r_input_edge|#r_input_raw
+;  y = address of byte to update
+joystick_update_byte:
+		sta	r_mask
+
+		lda	, x
+		bita	#INPUT_UP
+		beq	.up_not_pressed
+		inc	, y
+
+	.up_not_pressed:
+		bita	#INPUT_DOWN
+		beq	.down_not_pressed
+		dec	, y
+
+	.down_not_pressed:
+		bita	#INPUT_LEFT
+		beq	.left_not_pressed
+		ldb	, y
+		subb	#$10
+		stb	, y
+
+	.left_not_pressed:
+		bita	#INPUT_RIGHT
+		beq	.right_not_pressed
+		ldb	, y
+		addb	#$10
+		stb	, y
+	.right_not_pressed:
+		lda	, y
+		anda	r_mask
+		sta	, y
+		rts
+
+; Looking at input data, adjust the word in memory by
+;  +$1 for UP
+;  -$1 for DOWN
+; -$10 for LEFT
+; +$10 for RIGHT
+; Then apply the mask to the word
+; params:
+;  d = mask
+;  x = #r_input_edge|#r_input_raw
+;  y = address of word to update
+joystick_update_word:
+		std	r_mask
+
+		lda	, x
+		bita	#INPUT_UP
+		beq	.up_not_pressed
+		sta	r_scratch
+		ldd	, y
+		addd	#$1
+		std	, y
+		lda	r_scratch
+
+	.up_not_pressed:
+		bita	#INPUT_DOWN
+		beq	.down_not_pressed
+		sta	r_scratch
+		ldd	, y
+		subd	#$1
+		std	, y
+		lda	r_scratch
+
+	.down_not_pressed:
+		bita	#INPUT_LEFT
+		beq	.left_not_pressed
+		sta	r_scratch
+		ldd	, y
+		subd	#$10
+		std	, y
+		lda	r_scratch
+
+	.left_not_pressed:
+		bita	#INPUT_RIGHT
+		beq	.right_not_pressed
+		sta	r_scratch
+		ldd	, y
+		addd	#$10
+		std	, y
+		lda	r_scratch
+
+	.right_not_pressed:
+		ldd	, y
+		anda	r_mask
+		andb	r_mask + 1
+		std	, y
+		rts
 
 ; stall until the passed button is pressed
 ; params:
@@ -87,3 +187,4 @@ wait_button_release:
 ;		stb	r_scratch
 ;		adda	r_scratch
 r_scratch:		dcb.w 1
+r_mask:			dcb.w 1
