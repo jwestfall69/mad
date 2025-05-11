9e2c81f16c30090a34e1668767784045240e852c
Jim Westfall <commits@jwestfall.net>
2025-05-11 11:46:31 -0700

    vendetta: merge duplicate tile viewer code/data into shared functions part 2
    Forgot the actual tile viewer changes for the merged code, doh

--- machine/vendetta/main/src/viewers/fix_tile.asm
+++ machine/vendetta/main/src/viewers/fix_tile.asm
@@ -5,17 +5,19 @@
 	include "cpu/konami2/include/xy_string.inc"
 
 	include "machine.inc"
+	include "mad.inc"
 
 	global fix_tile_viewer
 
 	section code
 
-PALETTE_NUM		equ $1
 TILE_OFFSET_MASK	equ $0fff
 
 fix_tile_viewer:
 		RSUB	screen_init
-		bsr	fix_palette_setup
+
+		ldy	#FIX_TILE_PALETTE + (PALETTE_SIZE * TVC_PALETTE_NUM)
+		jsr	tvc_palette_setup
 
 		SEEK_XY	SCREEN_START_X, SCREEN_START_Y
 		ldy	#d_str_title
@@ -23,67 +25,14 @@ fix_tile_viewer:
 
 		ldd	#TILE_OFFSET_MASK
 		ldx	#fix_seek_xy_cb
-		ldy	#fix_draw_tile_cb
+		ldy	#tvc_draw_tile_cb
 		jsr	tile8_viewer_handler
 		rts
-
-; Palette Layout
-;  xBBB BBGG GGGR RRRR
-fix_palette_setup:
-
-
-		lda	#$1
-		sta	REG_CONTROL
-
-		ldx	#d_palette_data
-		ldy	#FIX_TILE_PALETTE + (PALETTE_SIZE*PALETTE_NUM)
-		ldb	#PALETTE_SIZE
-
-	.loop_next_color:
-		lda	,x+
-		sta	,y+
-		decb
-		bne	.loop_next_color
-
-		lda	#$0
-		sta	REG_CONTROL
-
-		rts
-
 
 fix_seek_xy_cb:
 		RSUB	screen_seek_xy
 		rts
 
-; params:
-;  d = tile (word)
-;  x = already at location in tile ram
-; PPTT ??TT TTTT TTTT
-fix_draw_tile_cb:
-		exg	b, a
-		RSUB	print_byte
-
-		tfr	b, a
-		bita	#$4
-		beq	.skip1
-		ora	#$10
-	.skip1:
-
-		bita	#$8
-		beq	.skip2
-		ora	#$20
-	.skip2:
-		anda	#$33
-
-		ora	#(PALETTE_NUM<<6)
-		sta	-$2000, x
-		rts
-
 	section data
-
-d_palette_data:
-	dc.w	$0000, $6d39, $3e34, $fe00, $5864, $8064, $e519
-	dc.w	$d126, $ffff, $8271, $0e78, $aa39, $f725, $0e5e
-	dc.w	$9532, $e277
 
 d_str_title: 	STRING "FIX TILE VIEWER"
--- machine/vendetta/main/src/viewers/layer_a_tile.asm
+++ machine/vendetta/main/src/viewers/layer_a_tile.asm
@@ -5,17 +5,19 @@
 	include "cpu/konami2/include/xy_string.inc"
 
 	include "machine.inc"
+	include "mad.inc"
 
 	global layer_a_tile_viewer
 
 	section code
 
-PALETTE_NUM		equ $1
 TILE_OFFSET_MASK	equ $0fff
 
 layer_a_tile_viewer:
 		RSUB	screen_init
-		bsr	layer_a_palette_setup
+
+		ldy	#LAYER_A_TILE_PALETTE + (PALETTE_SIZE * TVC_PALETTE_NUM)
+		jsr	tvc_palette_setup
 
 		SEEK_XY	SCREEN_START_X, SCREEN_START_Y
 		ldy	#d_str_title
@@ -23,33 +25,9 @@ layer_a_tile_viewer:
 
 		ldd	#TILE_OFFSET_MASK
 		ldx	#layer_a_seek_xy_cb
-		ldy	#layer_a_draw_tile_cb
+		ldy	#tvc_draw_tile_cb
 		jsr	tile8_viewer_handler
 		rts
-
-; Palette Layout
-;  xBBB BBGG GGGR RRRR
-layer_a_palette_setup:
-
-
-		lda	#$1
-		sta	REG_CONTROL
-
-		ldx	#d_palette_data
-		ldy	#LAYER_A_TILE_PALETTE + (PALETTE_SIZE*PALETTE_NUM)
-		ldb	#PALETTE_SIZE
-
-	.loop_next_color:
-		lda	,x+
-		sta	,y+
-		decb
-		bne	.loop_next_color
-
-		lda	#$0
-		sta	REG_CONTROL
-
-		rts
-
 
 layer_a_seek_xy_cb:
 		RSUB	screen_seek_xy
@@ -58,35 +36,6 @@ layer_a_seek_xy_cb:
 		leax	$800, x
 		rts
 
-; params:
-;  d = tile (word)
-;  x = already at location in tile ram
-; PPTT ??TT TTTT TTTT
-layer_a_draw_tile_cb:
-		exg	b, a
-		RSUB	print_byte
-
-		tfr	b, a
-		bita	#$4
-		beq	.skip1
-		ora	#$10
-	.skip1:
-
-		bita	#$8
-		beq	.skip2
-		ora	#$20
-	.skip2:
-		anda	#$33
-
-		ora	#(PALETTE_NUM<<6)
-		sta	-$2000, x
-		rts
-
 	section data
-
-d_palette_data:
-	dc.w	$0000, $6d39, $3e34, $fe00, $5864, $8064, $e519
-	dc.w	$d126, $ffff, $8271, $0e78, $aa39, $f725, $0e5e
-	dc.w	$9532, $e277
 
 d_str_title: 	STRING "LAYER A TILE VIEWER"
