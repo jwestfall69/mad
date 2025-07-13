2b30e8991dbdff1b39f85f81f1f15b4b861b396f
Jim Westfall <commits@jwestfall.net>
2025-07-12 18:51:58 -0700

    z80: add joystick_lr_update_(byte|word) functions

--- common/cpu/z80/src/util.asm
+++ common/cpu/z80/src/util.asm
@@ -6,6 +6,8 @@
 	ifnd _HEADLESS_
 
 	global joystick_update_byte
+	global joystick_lr_update_byte
+	global joystick_lr_update_word
 	global print_b2_return_to_menu
 	global print_passes
 	global sound_play_byte_dsub
@@ -67,6 +69,113 @@ joystick_update_byte:
 		ld	a, (iy)
 		and	b
 		ld	(iy), a
+		ret
+
+; Looking at joystick left/right, adjust the byte in memory
+; -$1 for LEFT
+; +$1 for RIGHT
+; If b1 is held down, then inc/dec is $10
+; Then apply the mask to the byte
+; params:
+;  a = mask
+;  ix = r_input_edge|r_input_raw
+;  iy = ptr to byte
+; returns:
+;  a = 0 no change, 1 = change
+joystick_lr_update_byte:
+		ld	(r_mask), a
+
+		ld	b, (ix)
+		ld	ix, r_scratch	; using for inc/dec amount
+		ld	(ix), $1
+
+		; don't allow $10 inc/dec mount if mask wouldn't allow
+		; a value of >= $10
+		cp	$10
+		jp	c, .b1_not_pressed
+
+		ld	a, (r_input_raw)
+		bit	INPUT_B1_BIT, a
+		jr	z, .b1_not_pressed
+		ld	(ix), $10
+
+	.b1_not_pressed:
+		bit	INPUT_LEFT_BIT, b
+		jr	z, .left_not_pressed
+		ld	a, (iy)
+		sub	(ix)
+		ld	(iy), a
+		jr	.apply_mask_return
+
+	.left_not_pressed:
+		bit	INPUT_RIGHT_BIT, b
+		jr	z, .return_no_change
+		ld	a, (iy)
+		add	a, (ix)
+		ld	(iy), a
+
+	.apply_mask_return:
+		ld	ix, r_mask
+		ld	a, (iy)
+		and	(ix)
+		ld	(iy), a
+		ld	a, $1
+		ret
+
+	.return_no_change:
+		xor	a
+		ret
+
+; Looking at joystick left/right, adjust the byte in memory
+; -$1 for LEFT
+; +$1 for RIGHT
+; If b1 is held down, then inc/dec is $10
+; Then apply the mask to the byte
+; params:
+;  bc = mask
+;  ix = r_input_edge|r_input_raw
+;  iy = ptr to byte
+; returns:
+;  a = 0 no change, 1 = change
+joystick_lr_update_word:
+		ld	(r_mask), bc
+
+		ld	h, (iy + 1)
+		ld	l, (iy + 0)
+
+		ld	bc, $1
+
+		ld	a, (r_input_raw)
+		bit	INPUT_B1_BIT, a
+		jr	z, .b1_not_pressed
+		ld	bc, $10
+
+	.b1_not_pressed:
+		ld	a, (ix)
+		bit	INPUT_LEFT_BIT, a
+		jr	z, .left_not_pressed
+		or	a	; clear carry
+		sbc	hl, bc
+		jr	.apply_mask_return
+
+	.left_not_pressed:
+		bit	INPUT_RIGHT_BIT, a
+		jr	z, .return_no_change
+		add	hl, bc
+
+	.apply_mask_return:
+		ld	bc, (r_mask)
+		ld	a, h
+		and	b
+		ld	(iy + 1), a
+		ld	a, l
+		and	c
+		ld	(iy + 0), a
+		ld	a, $1
+		ret
+
+	.return_no_change:
+		xor	a
 		ret
 
 ; these 2 print function help de-dupe some common
