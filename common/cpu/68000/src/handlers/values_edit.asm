d4ebc9979bbb5afff8549202708fd90d040fae54
Jim Westfall <commits@jwestfall.net>
2025-07-19 21:38:34 -0700

    68000: add values edit handler; switch tmnt sprite viewer to it

---
+++ common/cpu/68000/src/handlers/values_edit.asm
@@ -0,0 +1,216 @@
+	include "cpu/68000/include/common.inc"
+	include "cpu/68000/include/handlers/values_edit.inc"
+
+	global values_edit_handler
+
+	section code
+
+DATA_START_COLUMN	equ SCREEN_START_X + 15
+DATA_START_ROW		equ SCREEN_START_Y + 2
+
+CURSOR_X_OFFSET		equ SCREEN_START_X - 1
+CURSOR_Y_OFFSET		equ SCREEN_START_Y + 2
+
+; params:
+;  a0 = ve_settings
+;  a1 = ve_list
+values_edit_handler:
+		move.l	a0, r_ve_settings
+		move.l	a1, r_ve_list
+
+		lea	d_screen_xys_list, a0
+		RSUB	print_xy_string_list
+		jsr	print_b2_return_to_menu
+
+		; save off as 0 indexed count to r_cursor_max
+		jsr	get_ve_entry_count
+		subq.b	#$1, d0
+		move.b	d0, r_cursor_max
+
+		clr.b	r_cursor
+		clr.b	r_cursor_prev
+
+	.loop_redraw_data:
+		jsr	print_data
+		move.l	r_ve_settings, a1
+		move.l	s_ves_value_changed_cb(a1), a1
+		jsr	(a1)
+
+	.loop_redraw_cursor:
+		; clear old
+		moveq	#CURSOR_X_OFFSET, d0
+		moveq	#CURSOR_Y_OFFSET, d1
+		add.b	r_cursor_prev, d1
+		RSUB	screen_seek_xy
+
+		move.b	#CURSOR_CLEAR_CHAR, d0
+		RSUB	print_char
+
+		; draw new
+		moveq	#CURSOR_X_OFFSET, d0
+		moveq	#CURSOR_Y_OFFSET, d1
+		add.b	r_cursor, d1
+		RSUB	screen_seek_xy
+
+		move.b	#CURSOR_CHAR, d0
+		RSUB	print_char
+
+		move.b	r_cursor, r_cursor_prev
+
+		; seek to the correct ve_entry based on the r_cursor
+		move.l	r_ve_list, a0
+		move.b	r_cursor, d0
+	.loop_next_ve_entry:
+		cmp.b	#$0, d0
+		beq	.loop_input
+		addq.l	#s_vee_struct_size, a0
+		subq.b	#$1, d0
+		bra	.loop_next_ve_entry
+
+	.loop_input:
+		WATCHDOG
+
+		move.l	r_ve_settings, a1
+		move.l	s_ves_loop_input_cb(a1), a1
+		movem.l	a0, -(a7)
+		jsr	(a1)
+		movem.l	(a7)+, a0
+
+		bsr	input_update
+		move.b	r_input_edge, d0
+
+		btst	#INPUT_UP_BIT, d0
+		beq	.up_not_pressed
+
+		subq.b	#$1, r_cursor
+		bpl	.loop_redraw_cursor
+		move.b	r_cursor_max, r_cursor
+		bra	.loop_redraw_cursor
+
+	.up_not_pressed:
+		btst	#INPUT_DOWN_BIT, d0
+		beq	.down_not_pressed
+
+		addq.b	#$1, r_cursor
+		move.b	r_cursor_max, d0
+		cmp.b	r_cursor, d0
+		bge	.loop_redraw_cursor
+		move.b	#$0, r_cursor
+		bra	.loop_redraw_cursor
+
+	.down_not_pressed:
+		btst	#INPUT_B2_BIT, d0
+		beq	.b2_not_pressed
+		rts
+
+	.b2_not_pressed:
+		movem.l	a0, -(a7)
+		move.l	a0, a1
+
+		move.b	s_vee_input(a1), d0
+		cmp.b	#VE_INPUT_EDGE, d0
+		beq	.input_edge
+		lea	r_input_raw, a0
+		bra	.input_setup_done
+
+	.input_edge:
+		lea	r_input_edge, a0
+
+	.input_setup_done:
+		move.w	s_vee_mask(a1), d0
+		move.b	s_vee_type(a1), d1
+		move.l	s_vee_address(a1), a1
+
+		cmp.b	#VE_TYPE_WORD, d1
+		beq	.type_word
+		jsr	joystick_lr_update_byte
+		bra	.check_value_change
+
+	.type_word:
+		jsr	joystick_lr_update_word
+
+	.check_value_change:
+		movem.l	(a7)+, a0
+		tst.b	d0
+		beq	.loop_input
+		bra	.loop_redraw_data
+
+; returns
+;  d0 = number of ve entries in the list
+get_ve_entry_count:
+		move.l	r_ve_list, a0
+		moveq	#$0, d0
+
+	.loop_next_entry:
+		move.b	s_vee_type(a0), d1
+		cmp.b	#$ff, d1
+		beq	.list_end
+		addq.b	#$1, d0
+		add.l	#s_vee_struct_size, a0
+		bra	.loop_next_entry
+
+	.list_end:
+		rts
+
+print_data:
+		move.b	#DATA_START_ROW, r_y_offset
+		move.l	r_ve_list, a0
+
+	.loop_next_ve_entry:
+		move.b	s_vee_type(a0), d0
+		cmp.b	#$ff, d0
+		beq	.list_end
+
+		add.b	#DATA_START_COLUMN, d0
+		move.b	r_y_offset, d1
+		RSUB	screen_seek_xy
+
+		move.l	s_vee_address(a0), a1
+
+		move.b	s_vee_type(a0), d0
+		cmp.b	#VE_TYPE_WORD, d0
+		beq	.print_word
+		cmp.b	#VE_TYPE_BYTE, d0
+		beq	.print_byte
+
+		move.b	(a1), d0
+		RSUB	print_hex_nibble
+		bra	.print_done
+
+	.print_byte:
+		move.b	(a1), d0
+		RSUB	print_hex_byte
+		bra	.print_done
+
+	.print_word:
+		move.w	(a1), d0
+		RSUB	print_hex_word
+
+	.print_done:
+		add.l	#s_vee_struct_size, a0
+		add.b	#$1, r_y_offset
+		bra	.loop_next_ve_entry
+
+	.list_end:
+		rts
+
+	section data
+	align 1
+
+d_screen_xys_list:
+	XY_STRING SCREEN_START_X, (SCREEN_B1_Y - 2), "UD - SWITCH ATTRIBUTE"
+	XY_STRING SCREEN_START_X, (SCREEN_B1_Y - 1), "LR - ADJUST VALUE"
+	XY_STRING SCREEN_START_X, SCREEN_B1_Y, "B1 - HOLD TO ADJUST 10X"
+	XY_STRING_LIST_END
+
+	section bss
+	align 1
+
+r_ve_list:		dcb.l 1
+r_ve_settings:		dcb.l 1
+
+r_y_offset:		dcb.b 1
+
+r_cursor:		dcb.b 1
+r_cursor_prev:		dcb.b 1
+r_cursor_max:		dcb.b 1
--- machine/tmnt/main/src/viewers/sprite.asm
+++ machine/tmnt/main/src/viewers/sprite.asm
@@ -1,31 +1,34 @@
 	include "cpu/68000/include/common.inc"
-	include "global/include/sprite/konami/k051960.inc"
+	include "cpu/68000/include/handlers/values_edit.inc"
 
 	global sprite_viewer
 	global sprite_viewer_palette_setup
 
 	section code
 
-SPRITE_NUM_MASK		equ $1fff
-
 sprite_viewer:
 		jsr	sprite_viewer_palette_setup
 
-		; init struct
-		lea	r_sprite_struct, a0
-		move.w	#$600, s_se_num(a0)
-		move.b	#$3, s_se_size(a0)
-		clr.b	s_se_zoom_x(a0)
-		clr.b	s_se_zoom_y(a0)
-		move.w	#$110, s_se_pos_x(a0)
-		move.w	#$b0, s_se_pos_y(a0)
+		lea	d_screen_xys_list, a0
+		RSUB	print_xy_string_list
+		jsr	print_b2_return_to_menu
 
-		move.w	#SPRITE_NUM_MASK, d0
-		lea	draw_sprite_cb, a1
-		jsr	sprite_k051960_viewer_handler
+		move.w	#$600, r_sprite_num
+		move.b	#$3, r_sprite_size
+		move.w	#$b0, r_sprite_pos_x
+		move.w	#$80, r_sprite_pos_y
+
+		clr.b	r_sprite_zoom_x
+		clr.b	r_sprite_zoom_y
+		clr.b	r_sprite_flip_x
+		clr.b	r_sprite_flip_y
+
+		lea	d_ve_settings, a0
+		lea	d_ve_list, a1
+		jsr	values_edit_handler
 		rts
 
-; Per MAME
+; Per MAME (k051960)
 ; * Sprite Format
 ; * ------------------
 ; *
@@ -34,8 +37,8 @@ sprite_viewer:
 ; *   0  | x------- | active (show this sprite)
 ; *   0  | -xxxxxxx | priority order
 ; *   1  | xxx----- | sprite size (see below)
-; *   1  | ---xxxxx | sprite code (high 5 bits)
-; *   2  | xxxxxxxx | sprite code (low 8 bits)
+; *   1  | ---xxxxx | sprite number (high 5 bits)
+; *   2  | xxxxxxxx | sprite number (low 8 bits)
 ; *   3  | xxxxxxxx | "color", but depends on external connections (see below)
 ; *   4  | xxxxxx-- | zoom y (0 = normal, >0 = shrink)
 ; *   4  | ------x- | flip y
@@ -45,53 +48,46 @@ sprite_viewer:
 ; *   6  | ------x- | flip x
 ; *   6  | -------x | x position (high bit)
 ; *   7  | xxxxxxxx | x position (low 8 bits)
-; *
-; * Example of "color" field for Punk Shot:
-; *   3  | x------- | shadow
-; *   3  | -xx----- | priority
-; *   3  | ---x---- | use second gfx ROM bank
-; *   3  | ----xxxx | color code
-; *
-; * shadow enables transparent shadows. Note that it applies to pen 0x0f ONLY.
-; * The rest of the sprite remains normal.
-; * Note that Aliens also uses the shadow bit to select the second sprite bank.
-draw_sprite_cb:
-		lea	r_sprite_struct, a0
-
+value_changed_cb:
 		move.b	#$ff, SPRITE_RAM
 
-		moveq	#$0, d0
-		move.b	s_se_size(a0), d0
-		lsl.w	#$5, d0
-		or.b	s_se_num(a0), d0
+		move.b	r_sprite_size, d0
+		lsl.b	#$5, d0
+		or.b	r_sprite_num, d0
 		move.b	d0, SPRITE_RAM + 1
 
-		move.w	s_se_num(a0), d0
-		move.b	d0, SPRITE_RAM + 2
+		move.b	r_sprite_num + 1, SPRITE_RAM + 2
 
 		clr.b	SPRITE_RAM + 3
 
-		move.b	s_se_zoom_y(a0), d0
+		move.b	r_sprite_zoom_y, d0
 		lsl.b	#$2, d0
-		or.b	s_se_pos_y(a0), d0
+		or.b	r_sprite_pos_y, d0
+		tst.b	r_sprite_flip_y
+		beq	.skip_sprite_flip_y
+		or.b	#$02, d0
+	.skip_sprite_flip_y:
 		move.b	d0, SPRITE_RAM + 4
+		move.b	r_sprite_pos_y + 1, SPRITE_RAM + 5
 
-		move.w	s_se_pos_y(a0), d0
-		move.b	d0, SPRITE_RAM + 5
-
-		move.b	s_se_zoom_x(a0), d0
+		move.b	r_sprite_zoom_x, d0
 		lsl.b	#$2, d0
-		or.b	s_se_pos_x(a0), d0
+		or.b	r_sprite_pos_x, d0
+		tst.b	r_sprite_flip_x
+		beq	.skip_sprite_flip_x
+		or.b	#$02, d0
+	.skip_sprite_flip_x:
 		move.b	d0, SPRITE_RAM + 6
+		move.b	r_sprite_pos_x + 1, SPRITE_RAM + 7
+		rts
 
-		move.w	s_se_pos_x(a0), d0
-		move.b	d0, SPRITE_RAM + 7
+loop_input_cb:
 		rts
 
 sprite_viewer_palette_setup:
 		lea	d_palette_data, a0
 		lea	SPRITE_PALETTE, a1
-		moveq	#PALETTE_SIZE / 2, d0
+		moveq	#(PALETTE_SIZE / 2) - 1, d0
 
 	.loop_next_byte:
 		addq.l	#$1, a1
@@ -102,11 +98,43 @@ sprite_viewer_palette_setup:
 	section data
 	align 1
 
+d_ve_settings:
+	VE_SETTINGS value_changed_cb, loop_input_cb
+
+d_ve_list:
+	VE_ENTRY VE_TYPE_WORD, VE_INPUT_EDGE, r_sprite_num, $1fff
+	VE_ENTRY VE_TYPE_NIBBLE, VE_INPUT_EDGE, r_sprite_size, $7
+	VE_ENTRY VE_TYPE_BYTE, VE_INPUT_EDGE, r_sprite_zoom_x, $3f
+	VE_ENTRY VE_TYPE_BYTE, VE_INPUT_EDGE, r_sprite_zoom_y, $3f
+	VE_ENTRY VE_TYPE_WORD, VE_INPUT_RAW, r_sprite_pos_x, $1ff
+	VE_ENTRY VE_TYPE_WORD, VE_INPUT_RAW, r_sprite_pos_y, $1ff
+	VE_ENTRY VE_TYPE_NIBBLE, VE_INPUT_EDGE, r_sprite_flip_x, $1
+	VE_ENTRY VE_TYPE_NIBBLE, VE_INPUT_EDGE, r_sprite_flip_y, $1
+	VE_LIST_END
+
 d_palette_data:
 	dc.w	$0000, $0000, $01a0, $028d, $0374, $018e, $01f2, $0276
 	dc.w	$029b, $6a55, $6611, $7ee0, $35ad, $5294, $6b5a, $2d6b
 
+d_screen_xys_list:
+	XY_STRING SCREEN_START_X, (SCREEN_START_Y + 2), "SPRITE NUM"
+	XY_STRING SCREEN_START_X, (SCREEN_START_Y + 3), "SIZE"
+	XY_STRING SCREEN_START_X, (SCREEN_START_Y + 4), "ZOOM X"
+	XY_STRING SCREEN_START_X, (SCREEN_START_Y + 5), "ZOOM Y"
+	XY_STRING SCREEN_START_X, (SCREEN_START_Y + 6), "POS X"
+	XY_STRING SCREEN_START_X, (SCREEN_START_Y + 7), "POS Y"
+	XY_STRING SCREEN_START_X, (SCREEN_START_Y + 8), "FLIP X"
+	XY_STRING SCREEN_START_X, (SCREEN_START_Y + 9), "FLIP Y"
+	XY_STRING_LIST_END
+
 	section bss
 	align 1
 
-r_sprite_struct:	dc.b s_se_struct_size
+r_sprite_num:		dcb.w 1
+r_sprite_pos_x:		dcb.w 1
+r_sprite_pos_y:		dcb.w 1
+r_sprite_size:		dcb.b 1
+r_sprite_zoom_x:	dcb.b 1
+r_sprite_zoom_y:	dcb.b 1
+r_sprite_flip_x:	dcb.b 1
+r_sprite_flip_y:	dcb.b 1
